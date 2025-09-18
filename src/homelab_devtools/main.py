36af71971b7a77874da958f625b8546305ed8a98
snow-man-1 <snowman.tech@proton.me>
2025-09-18 17:56:17 +0200

    feat: add start function for the cli app

--- src/homelab_devtools/main.py
+++ src/homelab_devtools/main.py
@@ -2,3 +2,18 @@
 """
 Author: snow-man-1
 """
+
+from typer import Typer
+
+from homelab_devtools.cli_factory import CliFactory
+
+
+def start_cli() -> None:
+    factory = CliFactory()
+
+    cli_app: Typer = factory.create_cli_app()
+    cli_app()
+
+
+if __name__ == "__main__":
+    start_cli()
