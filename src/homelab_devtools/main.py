c943c5bb4d2a58985551a5b3e4078853d4ebedae
snow-man-1 <snowman.tech@proton.me>
2025-10-12 13:40:37 +0200

    feat: add logging setup with a factory

--- src/homelab_devtools/main.py
+++ src/homelab_devtools/main.py
@@ -6,9 +6,12 @@ Author: snow-man-1
 from typer import Typer
 
 from src.homelab_devtools.cli_factory import CliFactory
+from src.homelab_devtools.logger_factory import LoggerFactory
 
 
 def start_cli() -> None:
+    LoggerFactory.setup_logger()
+
     factory = CliFactory()
 
     cli_app: Typer = factory.create_cli_app()
